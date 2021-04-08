bf2cde7b466a69d778eb6aca886997e42eb36d6b
Cara Giulioni <caragiulioni@gmail.com>
2021-04-08 15:52:25 -0400

    add BE skeleton

---
+++ server/routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+//import HANDLERS
+
+//router.get("/", handlerFunction);
+//router.get("/path/:param", handleParam);
+
+module.exports = router;
