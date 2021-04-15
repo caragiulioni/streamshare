d3ae5c9427244964647584c1988f91b2dfdc4051
Cara Giulioni <caragiulioni@gmail.com>
2021-04-15 10:10:23 -0400

    add folder fix

---
+++ server/handlers/titleHandlers.js
@@ -0,0 +1,30 @@
+const request = require("request");
+// const { MongoClient, ObjectID } = require("mongodb");
+require("dotenv").config();
+const { API_SECRET } = process.env;
+// const options = { useNewUrlParser: true, useUnifiedTopology: true };
+
+const getTitle = (req, res) => {
+  const titleId = req.params.titleId;
+  const request = require("request");
+
+  const options = {
+    method: "GET",
+    url: "https://movie-database-imdb-alternative.p.rapidapi.com/",
+    qs: { i: `${titleId}`, r: "json" },
+    headers: {
+      "x-rapidapi-key": `${API_SECRET}`,
+      "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+      useQueryString: true,
+    },
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    return res
+      .status(200)
+      .json({ status: 200, data: JSON.parse(body), message: "success" });
+  });
+};
+
+module.exports = { getTitle };
