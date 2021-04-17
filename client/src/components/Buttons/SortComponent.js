8680b28cc24ee290fea34497ae91c7acd690caa6
Cara Giulioni <caragiulioni@gmail.com>
2021-04-17 18:09:36 -0400

    add function clean up

---
+++ client/src/components/Buttons/SortComponent.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+const SortComponent = ({ titles, setTitles, original }) => {
+  const [prev, setPrev] = useState();
+
+  //simulate navlink behavior on button click
+  const remove = (prev) => {
+    document.getElementById(prev).classList.remove("active");
+  };
+  const addActive = (id) => {
+    document.getElementById(id).classList.add("active");
+  };
+
+  const descend = (prev, id) => {
+    prev && remove(prev);
+    addActive(id);
+    setPrev(id);
+    const arr = [...titles].sort((a, b) => a.Title.localeCompare(b.Title));
+    return setTitles(arr);
+  };
+
+  const ascend = (prev, id) => {
+    prev && remove(prev);
+    addActive(id);
+    setPrev(id);
+    const arr = [...titles].sort((a, b) => b.Title.localeCompare(a.Title));
+    return setTitles(arr);
+  };
+
+  const lastToFirst = (prev, id) => {
+    prev && remove(prev);
+    addActive(id);
+    setPrev(id);
+    const arr = [...titles].reverse();
+    return setTitles(arr);
+  };
+
+  const revert = (prev, id) => {
+    prev && remove(prev);
+    addActive(id);
+    setPrev(id);
+    setTitles(original);
+  };
+  return (
+    <Wrapper>
+      <button id="ascend" onClick={() => descend(prev, "ascend")}>
+        Z-A
+      </button>
+      <button id="descend" onClick={() => ascend(prev, "descend")}>
+        A-Z
+      </button>
+      <button id="last-first" onClick={() => lastToFirst(prev, "last-first")}>
+        last to recent
+      </button>
+      <button id="revert" onClick={() => revert(prev, "revert")}>
+        recent to last
+      </button>
+    </Wrapper>
+  );
+};
+
+export default SortComponent;
+
+const Wrapper = styled.div`
+  button {
+    cursor: pointer;
+    background-color: transparent;
+    border: none;
+    color: white;
+  }
+
+  .active {
+    color: yellow;
+  }
+`;
