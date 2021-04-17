50c2bbd7d8e609a622c5cc3db94f9f3a79081000
Cara Giulioni <caragiulioni@gmail.com>
2021-04-17 06:54:11 -0400

    fix link accessibility

--- client/src/components/Title/index.js
+++ client/src/components/Title/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import kitten from "../../assets/kitten.jpg";
 import ActionBar from "../ActionBar";
 const Title = ({ result }) => {
-  let history = useHistory();
+  // let history = useHistory();
   let titleId = result.imdbID;
 
   const addDefaultSrc = (ev) => {
@@ -12,19 +12,19 @@ const Title = ({ result }) => {
   };
 
   const handleTitleClick = () => {
-    history.push(`title/${titleId}`);
+    // history.push(`title/${titleId}`);
   };
   const { Title, Poster, Year, Type } = result;
   return (
     <TitleContainer>
-      <ItemLink onClick={handleTitleClick}>
+      <Link exact to={`title/${titleId}`}>
         <h4>{Title}</h4>
         <Details>
           <p>{Year}</p>
           <p>{Type}</p>
         </Details>
         <img onError={addDefaultSrc} src={Poster} alt={Title} />
-      </ItemLink>
+      </Link>
     </TitleContainer>
   );
 };
@@ -42,8 +42,6 @@ const TitleContainer = styled.div`
     cursor: pointer;
   }
 `;
-
-const ItemLink = styled.div``;
 
 const Details = styled.div`
   display: flex;
