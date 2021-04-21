c97e74e161d78beeb15b533f36a766d3b9bce90f
Cara Giulioni <caragiulioni@gmail.com>
2021-04-21 11:32:49 -0400

    search text render

--- client/src/components/Search/Results.js
+++ client/src/components/Search/Results.js
@@ -8,7 +8,15 @@ const Results = () => {
   return (
     <ResultsSection>
       {response === "loading" && <Spinner />}
-      {response === "False" && <div>We couldn't find anything.</div>}
+      {response === "False" && (
+        <NoResult>
+          <p>Hmmm...we couldn't find anything!</p>
+          <p>
+            Try expanding your search Ex. queen's gambit instead of queens
+            gambit or law &amp; order instead of law and order
+          </p>
+        </NoResult>
+      )}
       {results && <ResultsContainer results={results} />}
     </ResultsSection>
   );
@@ -20,4 +28,21 @@ const ResultsSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const NoResult = styled.div`
+  width: 70%;
+  p {
+    font-size: 0.8em;
+    margin: 5px 0px;
+  }
+
+  p:first-of-type {
+    text-align: center;
+    margin: 10px;
+    color: var(--blue);
+  }
+  @media screen and (min-width: 700px) {
+    width: 30%;
+  }
 `;
